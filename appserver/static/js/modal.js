4769c0b0d07cd58ab6ce61e366451cfbc42e497c
Ryan <hodgsonr1234@gmail.com>
2019-10-18 10:38:45 +0800

    Double quotes -> Single quotes (es6 standard)

--- appserver/static/js/modal.js
+++ appserver/static/js/modal.js
@@ -1,14 +1,14 @@
 define(function (require, exports, module) {
-  const SearchManager = require("splunkjs/mvc/searchmanager");
-  const mvc = require("splunkjs/mvc");
+  const SearchManager = require('splunkjs/mvc/searchmanager');
+  const mvc = require('splunkjs/mvc');
   
   return {
     renderModal: function (context) {
       let identity = context.find('#identity').text();
       identity = retrieveIdentityString(identity);
 
-      $('#profile-modal').modal("show");
-      $('#ID').html("&nbsp" + identity);
+      $('#profile-modal').modal('show');
+      $('#ID').html('&nbsp' + identity);
 
       const historySearch = new SearchManager({
         preview: true,
@@ -16,7 +16,7 @@ define(function (require, exports, module) {
         search: mvc.tokenSafe("* is-ise cise_passed_authentications \"User-Name\" | where like(UserName,\"$studentid$\") | eval MAC=mvindex(split(Acct_Session_Id, \"/\"), 1) | table UserName MAC")
       });
 
-      const historyResults = historySearch.data("results");
+      const historyResults = historySearch.data('results');
       processResults(historyResults);
     },
   }
@@ -25,18 +25,18 @@ define(function (require, exports, module) {
 // HELPER FUNCTIONS
 
 function retrieveIdentityString(identityHTML) {
-  return identityHTML.split(":")[1].trim();
+  return identityHTML.split(':')[1].trim();
 }
 
 function processResults(historyResults) {
-  historyResults.on("data", function () {
+  historyResults.on('data', function () {
     const resultArray = historyResults.data().rows;
     const MACAddresses = resultArray.map(x => x[1])
 
     const html = MACAddresses.reduce((acc, x) => {
       const div = "<div id = \"MAC-address\">" + x + "</div>";
       return acc + div;
-    }, " ");
+    }, ' ');
     
     $('#history').html(html);
   });
--- appserver/static/js/searchbyidview.js
+++ appserver/static/js/searchbyidview.js
@@ -9,37 +9,37 @@ require([
 
   // Render text input for the student id input
   new TextInputView({
-    id: "txtNum",
-    value: mvc.tokenSafe("$studentid$"),
-    el: $("#studentid")
+    id: 'txtNum',
+    value: mvc.tokenSafe('$studentid$'),
+    el: $('#studentid')
   }).render()
 
   // Render date input for from date
   new TextInputView({
-    id: "from",
-    el: $("#from"),
-    type: "date",
-    value: mvc.tokenSafe("$fromdate$")
+    id: 'from',
+    el: $('#from'),
+    type: 'date',
+    value: mvc.tokenSafe('$fromdate$')
   }).render();
 
   // Render date input for to date
   new TextInputView({
-    id: "to",
-    el: $("#to"),
-    type: "date",
-    value: mvc.tokenSafe("$toDate$")
+    id: 'to',
+    el: $('#to'),
+    type: 'date',
+    value: mvc.tokenSafe('$toDate$')
   }).render();
 
   // Create view which contains IDs
   new ProfilesView({
-    id: "ProfilesView",
-    managerid: "searchForProfile",
-    el: $("#profilesView")
+    id: 'ProfilesView',
+    managerid: 'searchForProfile',
+    el: $('#profilesView')
   }).render();
 
   // Search Engine
   new SearchManager({
-    id: "searchForProfile",
+    id: 'searchForProfile',
     preview: true,
     cache: true,
     search: mvc.tokenSafe("* is-ise cise_passed_authentications \"User-Name\" | where like(UserName, \"$studentid$\") | eval MAC=mvindex(split(Acct_Session_Id, \"/\"), 1) | head 1 | table UserName MAC"),
