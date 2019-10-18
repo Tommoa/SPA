61d1a1a7d5cca409e6e317dfb66b4adb55b18667
Ryan <hodgsonr1234@gmail.com>
2019-10-18 14:54:17 +0800

    Added in SearchByMACAddress

---
+++ appserver/static/js/searchbyMACview.js
@@ -0,0 +1,61 @@
+require([
+  "/static/app/SPA/js/retrieveProfiles.js",
+  "/static/app/SPA/js/modal.js",
+  "splunkjs/mvc",
+  "splunkjs/mvc/searchmanager",
+  "splunkjs/mvc/textinputview",
+  'splunkjs/mvc/tokenutils',
+  "splunkjs/mvc/simplexml/ready!"
+], function (ProfilesView, Modal, mvc, SearchManager, TextInputView, TokenUtils) {
+
+  const tokens = mvc.Components.get('default');
+
+  // Render text input for the mac address input
+  new TextInputView({
+    id: 'txtNum',
+    value: mvc.tokenSafe('$MACaddress$'),
+    el: $('#MACaddress')
+  }).render()
+
+  // Render date input for from date
+  new TextInputView({
+    id: 'from',
+    el: $('#from'),
+    type: 'date',
+    value: mvc.tokenSafe('$earliest$'),
+  }).render();
+
+  // Render date input for to date
+  new TextInputView({
+    id: 'to',
+    el: $('#to'),
+    type: 'date',
+    value: mvc.tokenSafe('$latest$'),
+  }).render();
+
+  // Create view which contains IDs
+  new ProfilesView({
+    id: 'ProfilesView',
+    managerid: 'searchForProfile',
+    el: $('#profilesView')
+  }).render();
+
+  // Search Engine
+  new SearchManager({
+    id: 'searchForProfile',
+    preview: true,
+    cache: true,
+    search: mvc.tokenSafe("* is-ise cise_passed_authentications earliest=\"$earliest$\" latest=\"$latest$\" timeformat=\"%Y-%m-%d\" \"User-Name\" | eval MAC=mvindex(split(Acct_Session_Id, \"/\"), 1)| where like(MAC,\"$MACaddress$\") | head 1 | table UserName MAC"),
+  });
+
+  // Show Modal
+  $('#profilesView').on('click', function (event) {
+    let identity = $(this).find('#identity').text();
+    identity = identity.split(":")[1].trim();
+    const toDate = tokens.get('earliest');
+    const fromDate = tokens.get('latest');
+    Modal.renderModal(identity, toDate, fromDate);
+  });
+
+});
+
