28d6ea8cda5e3245acd182c98e2089e0aa0d7896
khuang247 <31398253+khuang247@users.noreply.github.com>
2020-04-11 18:10:09 +0800

    Add Contact List (#26)
    A CSV can be exported from Callista, which contains the name and contact information of students. This can be integrated into our profiles.
    
    * Added Contact List
    
    Added Contact List view page
    Added mycontact KV store
    Added contacts_csv folder monitor
    Added contacts_csv index
    Added contact details lookup to modal
    dedup MAC addresses on modal
    
    * Fixed whitespaces
    
    * Remove search time range

---
+++ appserver/static/js/contactlistview.js
@@ -0,0 +1,74 @@
+require([
+        "splunkjs/mvc",
+        "jquery",
+        "splunkjs/mvc/tableview",
+        "splunkjs/mvc/simpleform/input/text",
+        "splunkjs/mvc/simpleform/input/dropdown",
+        "splunkjs/mvc/simpleform/input/submit",
+        "splunkjs/mvc/searchmanager",
+        "splunkjs/mvc/simplexml/ready!"
+    ],
+    function(
+        mvc,
+        $,
+        TableView,
+        TextInput,
+        DropdownInput,
+        SubmitButton,
+        SearchManager
+    ) {
+
+        //
+        // SEARCH MANAGERS
+        //
+
+        var searchContactList = new SearchManager({
+            "id": "searchContactList",
+            "search": " | inputlookup mycontacts_lookup | eval  ID = _key | table ID, LastName, FirstName, Email, PhoneNumber",
+            "preview": true
+        }, { tokens: true });
+
+        var addCsvContactList = new SearchManager({
+            "id": "addCsvContactList",
+            "autostart": "false",
+            "search": `index="contacts_csv"
+                        | dedup id
+                        | rename first_name as FirstName, last_name as LastName, email as Email, phone_number as PhoneNumber
+                        | table id, LastName, FirstName, Email, PhoneNumber
+                        | outputlookup mycontacts_lookup append=T key_field=id`
+        });
+
+
+        //
+        // DISPLAY TABLE
+        //
+
+        var element1 = new TableView({
+            "id": "element1",
+            "drilldown": "none",
+            "managerid": "searchContactList",
+            "el": $('#display')
+        }, { tokens: true }).render();
+
+        //
+        // SERVICE OBJECT
+        //
+
+        // Create a service object using the Splunk SDK for JavaScript
+        // to send REST requests
+        var service = mvc.createService({ owner: "nobody" });
+
+        //
+        // SUBMIT FORM DATA
+        //
+
+        $(".addCsvButton").on("click", function() {
+            var ok = confirm("Add data from CSV folder?");
+            if (ok) {
+                addCsvContactList.startSearch();
+                alert('Data added!');
+                searchContactList.startSearch();
+            }
+        });
+
+    });
--- appserver/static/js/modal.js
+++ appserver/static/js/modal.js
@@ -8,11 +8,13 @@ define(function (require, exports, module) {
       $('#ID').html('&nbsp' + identity);
 
       const searchString = `
-        * is-ise cise_passed_authentications 
-        earliest="${fromDate}" latest="${toDate}" timeformat=\"%Y-%m-%d\" "User-Name" 
-        | where like(UserName, "${identity}") 
-        | eval MAC=mvindex(split(Acct_Session_Id, "/"), 1) 
-        | table UserName MAC
+        * is-ise cise_passed_authentications
+        earliest="${fromDate}" latest="${toDate}" timeformat=\"%Y-%m-%d\" "User-Name"
+        | where like(UserName, "${identity}")
+        | eval MAC=mvindex(split(Acct_Session_Id, "/"), 1)
+        | dedup MAC
+        | lookup mycontacts_lookup _key as UserName OUTPUT FirstName, LastName, Email, PhoneNumber
+        | table UserName, MAC, FirstName, LastName, Email, PhoneNumber
       `;
 
       const historySearch = new SearchManager({
@@ -31,7 +33,11 @@ define(function (require, exports, module) {
 function processResults(historyResults) {
   historyResults.on('data', function () {
     const resultArray = historyResults.data().rows;
-    const MACAddresses = resultArray.map(x => x[1])
+    const MACAddresses = resultArray.map(x => x[1]);
+    const first_name = resultArray.map(x => x[2], y => y[0]);
+    const last_name = resultArray.map(x => x[3], y => y[0]);
+    const email = resultArray.map(x => x[4], y => y[0]);
+    const phone_number = resultArray.map(x => x[5], y => y[0]);
 
     const html = MACAddresses.reduce((acc, x) => {
       const div = "<div id = \"MAC-address\">" + x + "</div>";
@@ -39,5 +45,10 @@ function processResults(historyResults) {
     }, ' ');
 
     $('#history').html(html);
+    $('#FirstName').html('First Name: ' + first_name);
+    $('#LastName').html('Last Name: ' + last_name);
+    $('#Email').html('Email Address: ' + email);
+    $('#PhoneNumber').html('Phone Number: ' + phone_number);
+
   });
 }
